36593391be150ecc98bedaddf53ccc6ff9e57ead
safeplace12345 <safeplace12345@gmail.com>
2021-01-17 13:57:07 +0200

    refactorised the addTodo

--- Fetch-API and CSS viewport/src/data/todos.js
+++ Fetch-API and CSS viewport/src/data/todos.js
@@ -2,7 +2,7 @@ import { data } from "../querySelector";
 
 import {URL} from "../querySelector"
 let dataBase = []; 
-const init = () => {
+const init = async() => {
      const response = await fetch(URL);
      dataBase = await response.json();
      return dataBase;
@@ -10,8 +10,15 @@ const init = () => {
 const getTodos = () => {
     return dataBase;
 }
- const addTodo = () =>{
-
+ const addTodo = (title , userId) =>{
+   const newTodo = {
+       userId,
+       title,
+       id : dataBase.length + 1,
+       completed : false
+   }
+   dataBase.push(newTodo);
+   return newTodo;
 }
  const deleteTodo =() => {
 
